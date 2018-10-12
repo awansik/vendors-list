110305c0c3a4d9b81aa1315c6d73d63b12601946
Andrew Wansik <amwansik@gmail.com>
2018-10-12 09:23:35 -0400

    added updates

---
+++ js/vendors-list.js
@@ -0,0 +1,25 @@
+$().ready(function() {
+
+    var url = "http://localhost:8080/Vendors/List";
+
+    $.getJSON(url)
+        .then(function(jsonResponse) {
+            console.log(jsonResponse);
+            render(jsonResponse);
+        });
+
+});
+
+function render(jsonResponse) {
+    var tbody = $("#tbody");
+    var vendors = jsonResponse.data;
+    for(var vendor of vendors) {
+        var row = "<tr>";
+        row += "<td>" + vendor.id + "</td>";
+        row += "<td>" + vendor.code + "</td>";
+        row += "<td>" + vendor.name + "</td>";
+        row += "<td>" + vendor.address + "</td>";
+        row += "</tr>";
+        tbody.append(row);
+    }
+}
